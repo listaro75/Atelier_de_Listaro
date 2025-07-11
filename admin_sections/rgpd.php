7473d3478c94a39c3574c41aa381b239514887e1
listaro75 <lucien.dacunha@gmail.com>
2025-07-11 16:09:38 +0200

    Ajout de la section RGPD pour le panel d'administration avec inclusion du centre de contrôle RGPD

---
+++ admin_sections/rgpd.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Section RGPD pour le panel d'administration
+ * Ce fichier est chargé dynamiquement par admin_panel.php
+ */
+
+// Inclure le centre de contrôle RGPD complet
+include_once 'rgpd_control.php';
+?>
