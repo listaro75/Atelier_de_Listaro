5c06cae213300d411f5346155795b7454b5d7bf2
listaro75 <lucien.dacunha@gmail.com>
2025-07-11 16:13:24 +0200

    Ajout de la section État du Système pour le panel d'administration

---
+++ admin_sections/system.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Section État du Système pour le panel d'administration
+ * Ce fichier est chargé dynamiquement par admin_panel.php
+ */
+
+// Inclure les statistiques système complètes
+include_once 'system_stats.php';
+?>
